1d5a6082c2d1a2d806ecd447c0621ee27fe48b2d
Ankit-Goswami <ankitgoswami.work@gmail.com>
2022-04-07 19:42:38 +0530

    query param task

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const router = express.Router();
 const logger = require('./logger')
 
-const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+
+router.get('/candidate', function (req, res) {
     console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    let arr = ["Aaran", "Aaren", "Aarez", "Aarman", "Aaron", "Aaron-James", "Aarron", "Aaryan", "Aaryn", "Aayan", "Aazaan", "Abaan", "Abbas", "Abdallah", "Abdalroof", "Abdihakim", "Abdirahman", "Abdisalam", "Abdul", "Abdul-Aziz", "Abdulbasir", "Abdulkadir", "Abdulkarem", "Abdulkhader", "Abdullah", "Abdul-Majeed", "Abdulmalik", "Abdul-Rehman", "Abdur", "Abdurraheem", "Abdur-Rahman", "Abdur-Rehmaan", "Abel", "Abhinav", "Abhisumant"] ;
+    let count = req.query.count
+    console.log(count);
+    let newArr = [];
+    if(count <= 0 || count >= arr.length){
+         res.status(404).send("Invalid Number");
+    }
+    else{
+        for(let i  = 0 ; i < count ; i++){
+            newArr.push(arr[i]);
+        }
+        // console.log(req)
+        // console.log('------------------')
+        // console.log('These are the request query parameters: ', req.query)
+         res.send(newArr);
+
+    }
+    
 });
 
 
