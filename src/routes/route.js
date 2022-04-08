a56807db52946d964245d9039bad89d27b0bf2d2
Ankit-Goswami <ankitgoswami.work@gmail.com>
2022-04-08 08:17:49 +0530

    7 apr aaignment GET resquest assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,15 +9,91 @@ router.get('/user-profile/:abcd', function(req, res) {
     res.send('dummy response')
 })
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+// router.get('/test-me', function (req, res) {
+//     console.log('------------------')
+//     console.log(req)
+//     console.log('------------------')
+//     console.log('These are the request query parameters: ', req.query)
+//     res.send('My first ever api!')
+// });
+
+router.get('/movies', function (req, res) { 
+    let movies = [ 'Heathers','Europa Report','Fellowship of the Ring', 'Silver Linings Playbook', 'Forest Gump',"Harry Potter","Titanic","Best Worst Movie","Troll 2" ];
+    
+    res.send(movies)
+});
+
+router.get('/movies/:indexNumber', function (req, res) { 
+    let movies = [ 'Heathers','Europa Report','Fellowship of the Ring', 'Silver Linings Playbook', 'Forest Gump',"Harry Potter","Titanic","Best Worst Movie","Troll 2" ];
+    
+    let index = req.params.indexNumber
+    
+    console.log(index);
+    if(index < 0 || index >= movies.length)  {
+         res.status(404).send("invalid index")
+    }
+    else{
+        for(let i = 0 ; i < movies.length ; i++){
+  
+            if(i == index){
+                 res.send(movies[index]);
+            }
+        }
+      
+    }   
+});
+
+router.get('/films', function (req, res) { 
+    //let movies = [ 'Heathers','Europa Report','Fellowship of the Ring', 'Silver Linings Playbook', 'Forest Gump',"Harry Potter","Titanic","Best Worst Movie","Troll 2" ];
+    let movies = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, 
+       {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+        
+       }]
+       
+    res.send(movies)
 });
 
 
+router.get('/films/:filmId', function (req, res) { 
+    //let movies = [ 'Heathers','Europa Report','Fellowship of the Ring', 'Silver Linings Playbook', 'Forest Gump',"Harry Potter","Titanic","Best Worst Movie","Troll 2" ];
+    
+    let filmId = req.params.filmId
+    
+    let movies = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, 
+       {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+        
+       }]
+
+       movies.map(function(obj) {
+           if(obj.id == filmId){
+               return res.send(obj);
+           }
+       })
+       
+    res.status(404).send('No movie exists with this id')
+});
 
 
 module.exports = router;
